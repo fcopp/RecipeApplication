30bb82c418d4c98ebc52a8f1b41dead46f63c945
fcopp <fcopp@uoguelph.ca>
2018-06-24 22:17:21 -0400

    attempting to add tables

--- backend.py
+++ backend.py
@@ -2,7 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect("database/database.db")
 
-cursor = connection.cursor()
+c = connection.cursor()
 
-#Create table
-c.execute("""CREATE TABLE )
+#Create ingredients
+c.execute("""CREATE TABLE IF NOT EXISTS ingredients (ingredientID INTEGER PRIMARY KEY, name TEXT)""")
+c.execute("""CREATE TABLE IF NOT EXISTS recipes (recipeID INTEGER PRIMARY KEY, name TEXT, instructions TEXT)""")
+c.execute("""CREATE TABLE IF NOT EXISTS recipeingredients (recipeID INT FOREIGN KEY references recipes.recipeID, ingredientID INT FOREIGN KEY references ingredients.ingredientID, quantity TEXT)""")
+
